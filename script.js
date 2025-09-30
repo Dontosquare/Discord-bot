e8d2034591acb811abc07f89201a47c08767dba7
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-09-30 16:31:39 +0200

    Update index.js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+
+client.on('messageCreate', async msg => {
+    if (msg.content === 'Good Boy') {
+        try {
+            const response = await fetch('https://dog.ceo/api/breeds/image/random');
+            const data = await response.json();
+            msg.reply(data.message);
+        } catch (error) {
+            msg.reply('Could not fetch a dog picture right now.');
+        }
+    }
+});
+
+client.login(process.env.TOKEN)
+
